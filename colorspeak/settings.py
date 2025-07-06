3a7c667b8c2e1eac4b424dfa1a5a0624f128f03e
Arnav <pawar.arnav1333@gmail.com>
2025-07-06 21:13:27 +0530

    supposed to be final commit

--- colorspeak/settings.py
+++ colorspeak/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['colorspeak.onrender.com', '127.0.0.1', 'localhost']
 
