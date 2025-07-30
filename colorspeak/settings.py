2b4f16772d9043390111998948588fce6419fe30
Arnav <pawar.arnav1333@gmail.com>
2025-07-30 21:27:14 +0530

    security commit

--- colorspeak/settings.py
+++ colorspeak/settings.py
@@ -60,7 +60,11 @@ MIDDLEWARE = [
     
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOWED_ORIGINS = [
+    "https://colorspeak.xyz",
+    "https://colorspeak.netlify.app/",
+    
+]
 ROOT_URLCONF = 'colorspeak.urls'
 
 TEMPLATES = [
